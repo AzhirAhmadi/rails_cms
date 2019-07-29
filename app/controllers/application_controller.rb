28eefbc8a84b6f2e9ee007f84780f2b04457ca79
AzhirAhmadi <aaahmadi99@gmail.com>
2019-07-29 10:22:29 +0430

    blobks problem solved

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,17 @@
 class ApplicationController < ActionController::Base
-    before_action :getPageNav
+    before_action :get_page_nav, :get_jumbotron, :get_fuck_ruby
 
     protect_from_forgery with: :exception
 
-    def getPageNav
+    def get_page_nav
         @pageNav =Page.where("menu_display = true && is_published = true").order(order: :asc)
+    end
+
+    def get_jumbotron
+        @jumbotron=Block.where("position = 'jumbotron'").order(order: :asc)
+    end
+
+    def get_fuck_ruby
+        @fuck_ruby=Block.where("position = 'block'").order(order: :asc)
     end
 end
